b8b1bfd8b68f38b4fdc148343a542be73f0be4ec
Yuri Moura <y2moura@hotmail.com>
2021-03-30 01:33:03 -0300

    Sistema de adicionar notes iniciado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import Sidebar from './Sidebar.js';
-import Notes from './Notes.js';
+import Note from './Notes.js';
+import { useState } from 'react';
 
 function App() {
+  const [noteState, setNotes] = useState([{color: 'blue'}]);
+
   return (
     <div className="app">
       <div className="container">
         <div className="row">
           <div className="side col-1">
-            <Sidebar />
+            <div className="Sidebar">
+              <p>Note it</p>
+              <div className="Add-note">
+                <button type="button" className="handle-buttons">+</button>
+                <button className="button red" 
+                type="button" 
+                onClick={() => {noteState.length<17 ? setNotes([...noteState, {color: 'red'}]) : console.log('limite')}}
+                />
+                <button className="button blue" 
+                type="button" 
+                onClick={() => {noteState.length<17 ? setNotes([...noteState, {color: 'blue'}]) : console.log('limite')}}
+                />
+
+                
+              </div>
+            </div>
           </div>
           <div className="notes col">
-            <Notes color="card color01" text="Preciso fazer uma anotação e usarei o Note it"/>
-            <Notes color="card color01" text="Preciso fazer uma anotação e usarei o Note it"/>
-            <Notes color="card color01" text="Preciso fazer uma anotação e usarei o Note it"/>
-            <Notes color="card color01" text="Preciso fazer uma anotação e usarei o Note it"/>
-            <Notes color="card color01" text="Preciso fazer uma anotação e usarei o Note it"/>
-            <Notes color="card color01" text="Preciso fazer uma anotação e usarei o Note it"/>
+            {
+            noteState
+            .map((note) => {
+              return <Note color={note.color} text={note.text}/>
+            })
 
+            }
           </div>
         </div>
       </div>
--- src/Notes.js
+++ src/Notes.js
@@ -2,11 +2,13 @@ import react from 'react';
 import './Note.css';
 
 function Notes(props) {
+  const cardClass = 'card '.concat(props.color);
+  const textClass = 'card-text '.concat(props.red);
     return (
-          <div className={props.color}>
+          <div className={cardClass}>
               <div className="card-body">
                 <div className="row01">
-                  <p className="card-text">{props.text}</p>
+                  <textarea className={textClass} maxLength="200" disabled="false" >{props.text}</textarea>
                 </div>
               </div>
           </div>
