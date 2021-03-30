dd7347a8d209e95c0d7d3097cc3522204e99866f
Yuri Moura <y2moura@hotmail.com>
2021-03-30 16:37:12 -0300

    Adicionado cores dos notes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import Note from './Notes.js';
 import { useState } from 'react';
 
 function App() {
-  const [noteState, setNotes] = useState([{color: 'blue'}]);
+  const [noteState, setNotes] = useState([{color: 'blue', id: '0'}]);
+
+  const carregarNote = (color) => {
+    noteState.length<=14 ? setNotes([...noteState, {color: `${color}`, id: `${noteState.length}`}]) : console.log('limite')
+  }
 
   return (
     <div className="app">
@@ -14,16 +18,23 @@ function App() {
             <div className="Sidebar">
               <p>Note it</p>
               <div className="Add-note">
-                <button type="button" className="handle-buttons">+</button>
-                <button className="button red" 
-                type="button" 
-                onClick={() => {noteState.length<17 ? setNotes([...noteState, {color: 'red'}]) : console.log('limite')}}
+                <button className="button red "
+                type="button"
+                onClick={() => carregarNote('red')}
                 />
                 <button className="button blue" 
                 type="button" 
-                onClick={() => {noteState.length<17 ? setNotes([...noteState, {color: 'blue'}]) : console.log('limite')}}
+                onClick={() => carregarNote('blue')}
                 />
-
+                <button className="button yellow" 
+                type="button"
+                onClick={() => carregarNote('yellow')}
+                />
+                <button className="button green" 
+                type="button"
+                onClick={() => carregarNote('green')}
+                />
+                
                 
               </div>
             </div>
@@ -32,7 +43,7 @@ function App() {
             {
             noteState
             .map((note) => {
-              return <Note color={note.color} text={note.text}/>
+              return <Note color={note.color} id={note.id} />
             })
 
             }
--- src/Notes.js
+++ src/Notes.js
@@ -1,14 +1,27 @@
 import react from 'react';
 import './Note.css';
 
+
 function Notes(props) {
   const cardClass = 'card '.concat(props.color);
-  const textClass = 'card-text '.concat(props.red);
+  const textClass = 'card-text '.concat(props.color);
+  const edit = () => {
+    const note = document.getElementById(`${props.id}text`)
+    note.getAttribute('disabled') ? note.removeAttribute('disabled') : note.setAttribute('disabled', 'disabled');
+    const button = document.getElementById(`${props.id}button`);
+    button.innerText == 'Save' ? button.innerText = 'Edit' : button.innerText = 'Save';
+  }
+
+
     return (
-          <div className={cardClass}>
+          <div className={cardClass} id={props.id}>
               <div className="card-body">
                 <div className="row01">
-                  <textarea className={textClass} maxLength="200" disabled="false" >{props.text}</textarea>
+                  <textarea className={textClass} id={`${props.id}text`} maxLength="180"  onDoubleClick={edit}></textarea>
+                </div>
+                <div className="row02">
+                  <button type="button" className="delete-button" id={props.id} onClick={deleteN} >Delete</button>
+                  <button type="button" className="edit-button" id={`${props.id}button`}onClick={edit}>Save</button>
                 </div>
               </div>
           </div>
